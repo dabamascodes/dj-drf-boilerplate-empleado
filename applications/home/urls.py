1fd649f7470e512ea58f008dc5b4e79d940ace3f
dabamascodes <daniel.6@live.com.mx>
2026-01-20 19:08:05 -0700

    29. Conexión Django con Postgresql

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
     path('home/', views.IndexView.as_view()),
     path('lista/', views.PruebaListView.as_view()),    
     path('lista-prueba/', views.ModeloPruebaListView.as_view()),    
