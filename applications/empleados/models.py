37c020312077d8c72126afc4bd1b354753567be2
dabamascodes <daniel.6@live.com.mx>
2026-01-22 18:03:27 -0700

    31. Class Meta, modelos y en el administrador Django

--- applications/empleados/models.py
+++ applications/empleados/models.py
@@ -18,5 +18,12 @@ class Empleado(models.Model):
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
     # image = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
     
+    
+    class Meta: 
+        verbose_name = 'Empleados'
+        verbose_name_plural = 'Mis Empleados'
+        ordering = ['-first_name']
+        unique_together = ('first_name', 'last_name')
+    
     def __str__(self):
         return str(self.id) + '-' + self.first_name + '-' + self.last_name
