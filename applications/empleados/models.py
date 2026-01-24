c027bb5ec6080c32d0d4e2d13854351542546287
dabamascodes <daniel.6@live.com.mx>
2026-01-23 17:08:28 -0700

    32. Personalizar Tablas en el Administrador Django

--- applications/empleados/models.py
+++ applications/empleados/models.py
@@ -2,6 +2,16 @@ from django.db import models
 from applications.departamento.models import Departamento
 
 # Create your models here.
+class Habilidades(models.Model):
+    habilidad = models.CharField('Habilidad', max_length=50)
+    
+    class Meta:
+        verbose_name = 'Habilidad'
+        verbose_name_plural = 'Habilidades Empleados'
+    
+    def __str__(self):
+        return str(self.id) + '-' + self.habilidad 
+
 class Empleado(models.Model):
     """ Modelo para tabla empleado """
     
@@ -17,13 +27,15 @@ class Empleado(models.Model):
     job = models.CharField('Trabajo', max_length=1, choices=JOB_CHOICES)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
     # image = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
+    # avatar = models.ImageField(upload_to='empleado', blank=True, null=True)
+    habilidades = models.ManyToManyField(Habilidades)
     
     
     class Meta: 
-        verbose_name = 'Empleados'
-        verbose_name_plural = 'Mis Empleados'
-        ordering = ['-first_name']
-        unique_together = ('first_name', 'last_name')
+        verbose_name = 'Mi Empleado'
+        verbose_name_plural = 'Empleados de la empresa'
+        ordering = ['-first_name', 'last_name']
+        unique_together = ('first_name', 'departamento')
     
     def __str__(self):
         return str(self.id) + '-' + self.first_name + '-' + self.last_name
