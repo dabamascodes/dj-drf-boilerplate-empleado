13f8c7e38c98f5885fee4a26c15be5a93d50c068
dabamascodes <daniel.6@live.com.mx>
2026-01-25 10:04:24 -0700

    33. Filtros en el Admin Django

--- applications/empleados/admin.py
+++ applications/empleados/admin.py
@@ -13,5 +13,9 @@ class EmpleadoAdmin(admin.ModelAdmin):
         'departamento',
         'job',
     )
+    search_fields = ("first_name",)
+    list_filter = ('job', 'habilidades')
+    
+    filter_horizontal = ('habilidades',)
 
 admin.site.register(Empleado, EmpleadoAdmin)
